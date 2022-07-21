18c07fe72a9c866bb5dcf0da1b98b50844c2e606
Madhana2019 <ermadhana94@gmail.com>
2022-07-20 22:06:45 -0700

    Project

---
+++ src/test/java/ort/test/aaa/Aaaaa.java
@@ -0,0 +1,12 @@
+package ort.test.aaa;
+
+public class Aaaaa {
+	
+	public static void main(String[] args) {
+		System.out.println("qwerty");
+		System.out.println("sdfghjk");
+		System.out.println("vghmnbvc");
+		System.out.println("okjhbvc");
+	}
+
+}
